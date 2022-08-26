8a7ab3ce1c05e8b0f8e1cee7d971c448f2c2a96e
Muhammad Sameer <m.sameer@arbisoft.com>
2022-08-26 16:36:22 +0500

    Created view for register page

--- listing/urls.py
+++ listing/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from listing.views import index
+from listing.views import index, register
 
 urlpatterns = [
     path('', index, name='index'),
+    path('register/', register, name='register')
 ]
--- listing/views.py
+++ listing/views.py
@@ -1,5 +1,24 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login, admin
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    return render(request, 'listing/layout.html')
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("/admin")
+
+        messages.warning(request, "Unsuccessful registration. Invalid information.")
+
+    form = UserCreationForm(request.POST)
+    return render(request, 'listing/register.html', context={"register_form": form})
