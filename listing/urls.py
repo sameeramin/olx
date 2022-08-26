e400aa3f017bd013da043109fe997cc3587dec89
Muhammad Sameer <m.sameer@arbisoft.com>
2022-08-26 19:17:18 +0500

    Added login functionality

--- listing/urls.py
+++ listing/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from listing.views import index, register
+from listing.views import index, register, log_in, log_out
 
 urlpatterns = [
     path('', index, name='index'),
-    path('register/', register, name='register')
+    path('register/', register, name='register'),
+    path('login/', log_in, name='log_in'),
+    path('logout/', log_out, name='log_out'),
 ]
--- listing/views.py
+++ listing/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import login, admin
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def index(request):
@@ -16,9 +16,38 @@ def register(request):
             user = form.save()
             login(request, user)
             messages.success(request, "Registration successful.")
-            return redirect("/admin")
+            return redirect("/")
 
         messages.warning(request, "Unsuccessful registration. Invalid information.")
 
-    form = UserCreationForm(request.POST)
+    if request.POST:
+        form = UserCreationForm(request.POST)
+    else:
+        form = UserCreationForm()
     return render(request, 'listing/register.html', context={"register_form": form})
+
+
+def log_in(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You're logged in as {username}.")
+                return redirect("/")
+            else:
+                messages.warning(request, "Invalid username or password.")
+        else:
+            messages.warning(request, "Invalid username or password.")
+
+    form = AuthenticationForm()
+    return render(request, 'listing/login.html', context={"login_form": form})
+
+
+def log_out(request):
+    pass
