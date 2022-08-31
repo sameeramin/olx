1f909d2c514b6429947427c3276977423805e72a
Muhammad Sameer <m.sameer@arbisoft.com>
2022-08-31 15:00:50 +0500

    Created Listing and other supporting models

--- listing/admin.py
+++ listing/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from listing.models import Listing, Category, City, Image
 
-# Register your models here.
+# Registering models
+admin.site.register(Listing)
+admin.site.register(Category)
+admin.site.register(City)
+admin.site.register(Image)
